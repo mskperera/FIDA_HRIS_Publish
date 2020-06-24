7835079119eb6af2e1ea1b3ad4f1ea0bdc9e4a18
spmskperera <spmskperera@gmail.com>
2020-06-24 19:04:38 +0530

    2020-06-24

---
+++ Model_Boxes/sMessageListBoxes/sMessageBox.js
@@ -0,0 +1,10 @@
+﻿
+    function HideMessageBox() {
+
+     
+    var x = document.getElementById("messagebox");
+    x.style.display = "none";
+    var bd = document.getElementById("backdropMessageBox");
+        bd.style.display = "none";
+
+}
